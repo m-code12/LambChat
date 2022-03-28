77a2a85f01fe79704c360f1d673143cfedd255d3
Monika Jha <monikajha70@gmail.com>
2022-03-28 23:37:28 +0530

    Add files via upload

---
+++ src/components/Login.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { GoogleOutlined } from '@ant-design/icons'
+import firebase from 'firebase/app'
+
+import { auth } from '../firebase';
+
+const Login = () => {
+    return (
+        <div id="login-page">
+            <div id="login-card">
+                <h2>Welcome to LambChat!</h2>
+                <div
+                    className="login-button google"
+                    onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+                >
+                    <GoogleOutlined /> Sign In With Google
+                </div>
+                <br /> <br />
+                
+            </div>
+        </div>
+    )
+}
+
+export default Login;
